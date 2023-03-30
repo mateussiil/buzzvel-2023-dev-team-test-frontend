4ce6373c5f6e0b878c6cf1adeaef28388f392017
Mateus Oliveira <mateussiiloliveira@gmail.com>
2023-03-29 21:40:38 -0300

    fix: fixed environment

--- utils/environment.ts
+++ utils/environment.ts
@@ -1,5 +1,6 @@
+const prod = process.env.NODE_ENV === 'production' 
 export const environment = {
   api:{
-    URLBase: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://www.pdfdesign.tech/laravel'
+    URLBase: process.env.NEXT_PUBLIC_BACKEND_URL || (prod ? 'https://www.pdfdesign.tech/laravel' : 'http://localhost:8989')
   }
 }
